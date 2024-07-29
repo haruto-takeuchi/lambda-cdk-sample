9d40814868ef3b152a16c0fd394a49d8c263ac82
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2024-07-30 00:17:41 +0900

    Lambda関数のリソースを定義

--- lib/lamdba-cdk-sample-stack.ts
+++ lib/lamdba-cdk-sample-stack.ts
@@ -1,6 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// Import Lambda L2 construct
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class LamdbaCdkSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +9,10 @@ export class LamdbaCdkSampleStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LamdbaCdkSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloWorldFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
   }
 }
