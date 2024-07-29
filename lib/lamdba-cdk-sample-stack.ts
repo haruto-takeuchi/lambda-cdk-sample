af82671c7a13b1254ed54ed842d25d80805ec0a2
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2024-07-30 00:25:34 +0900

    API Gatewayのリソース定義を追加

--- lib/lamdba-cdk-sample-stack.ts
+++ lib/lamdba-cdk-sample-stack.ts
@@ -2,17 +2,26 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 // Import Lambda L2 construct
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class LamdbaCdkSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
+    // Lambda Function resource
     const helloWorldFunction = new lambda.Function(this, "HelloWorldFunction", {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset("lambda"),
       handler: "hello.handler",
     });
+
+    // API Gateway resource
+    const api = new apigateway.LambdaRestApi(this, "HelloWorldApi", {
+      handler: helloWorldFunction,
+      proxy: false,
+    });
+
+    const helloResource = api.root.addResource("hello");
+    helloResource.addMethod("GET");
   }
 }
